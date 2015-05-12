752ac712f5b53679f724621ec7648e413d7a3853
Mike Talbott <mtalbott@harrislocalgov.com>
2015-05-11 20:03:45 -0400

    Project scaffold

---
+++ src/TalBot/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("TalBot")>]
+[<assembly: AssemblyProductAttribute("TalBot")>]
+[<assembly: AssemblyDescriptionAttribute("A bot for posting status messages to Slack and responding to Slack posts.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
