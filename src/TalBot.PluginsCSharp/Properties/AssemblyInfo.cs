2fdb0a0a02d8a902cc3b97cc325c40f1d2088237
Mike Talbott <mtalbott@harrislocalgov.com>
2015-06-10 12:18:22 -0400

    Bump version to 0.0.4-beta

--- src/TalBot.PluginsCSharp/Properties/AssemblyInfo.cs
+++ src/TalBot.PluginsCSharp/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("TalBot.PluginsCSharp")]
 [assembly: AssemblyProductAttribute("TalBot")]
 [assembly: AssemblyDescriptionAttribute("A bot for posting status messages to Slack and responding to Slack posts.")]
-[assembly: AssemblyVersionAttribute("0.0.3")]
-[assembly: AssemblyFileVersionAttribute("0.0.3")]
+[assembly: AssemblyVersionAttribute("0.0.4")]
+[assembly: AssemblyFileVersionAttribute("0.0.4")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.0.3";
+        internal const string Version = "0.0.4";
     }
 }
--- src/TalBot/AssemblyInfo.fs
+++ src/TalBot/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TalBot")>]
 [<assembly: AssemblyProductAttribute("TalBot")>]
 [<assembly: AssemblyDescriptionAttribute("A bot for posting status messages to Slack and responding to Slack posts.")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.3"
+    let [<Literal>] Version = "0.0.4"
